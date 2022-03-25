68a87e250b7f39fe7f35a2d7ba92ab01226229ff
westlove <ignaciowestlove@gmail.com>
2022-03-25 13:13:29 +0100

    slider carosel complete

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ function openNav() {
 
   }
   
+  var slideImg = document.getElementById("slideImg");
+  var images = new Array(
+      "/images/iphone132.png",
+      "/images/laptop1.png",
+      "/images/accessories1.png",
+    
+  );
+  var len = images.length;
+  var i = 0;
+  function slider(){
+      if(i> len-1){
+          i = 0;
+      }
+      slideImg.src = images[i];
+      i++;
+      setTimeout('slider()',3000);
+  }
